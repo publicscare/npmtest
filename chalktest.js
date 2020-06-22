5dcbb331950499bb1351f88122c25855dd251a78
Brian Crouse <113001@sncorp.intranet.com>
2020-06-22 09:55:52 -0500

    initial

---
+++ chalktest.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('########################################################'));
+console.log(chalk.green('RXDB node example'));
+console.log(chalk.green('########################################################\n \n'));
